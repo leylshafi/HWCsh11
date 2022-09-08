9ad0afdbce896b91c2ae93d52eb7d9e93c3e5de4
Leyla Shafiyeva <llshafi03@gmail.com>
2022-09-08 21:39:43 +0400

    Add project files.

---
+++ HWCsh11/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace HW11;
+
+delegate void Func(string str);
+
+class MyClass
+{
+
+
+    public string Str { get; set; }
+    public void Space(string str) => Console.WriteLine(string.Join('_', str.ToCharArray()));
+    public void Reverse(string str)
+    {
+        char[] temp = str.ToCharArray();
+        Array.Reverse(temp);
+        string result = new(temp);
+        Console.WriteLine(result);
+    }
+
+    public MyClass(string str)
+    {
+        Str = str;
+    }
+
+}
+
+class Run
+{
+    public void RunFunc(Func del, string str)
+    {
+        foreach (var func in del.GetInvocationList())
+            func.DynamicInvoke(str);
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Enter string");
+
+        var str = Console.ReadLine();
+        MyClass cls = new MyClass(str);
+
+        Func funcDell = null!;
+        funcDell += cls.Space;
+        funcDell += cls.Reverse;
+
+
+        Run run = new Run();
+        run.RunFunc(funcDell, str);
+    }
+}
